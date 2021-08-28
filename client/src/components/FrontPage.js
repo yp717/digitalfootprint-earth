1e2a76e91d66a815e6a6d27e9ec88e253d760e9a
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-08-28 14:32:39 +0100

    feat(story): added ping and mobile responsiveness

--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -11,13 +11,9 @@ import { Link as ScrollLink } from "react-scroll"
 import DevPost from "../assets/Devpost"
 import useAnimationFrame from "../utils/useAnimationFrame"
 import { useStory } from "../context/story-context"
+import { useWindowSize } from "../utils/useWindowSize"
 
 const ClientSideOnlyMap = React.lazy(() => import("./Map"))
-
-const variants = {
-  idle: { padding: "6rem" },
-  active: { padding: " 0rem" },
-}
 
 const FrontPage = ({ children }) => {
   const isSSR = typeof window === "undefined"
@@ -34,6 +30,11 @@ const FrontPage = ({ children }) => {
   } = useStory()
   const [hovered, setHovered] = useState("")
   const [userInput, setUserInput] = useState("")
+  const { width } = useWindowSize()
+  const variants = {
+    idle: { padding: width < 550 ? "2rem" : width < 1000 ? "4rem" : "6rem" },
+    active: { padding: " 0rem" },
+  }
 
   useAnimationFrame(() => {
     if (mapRef.current && mapRef.current.camera) {
@@ -48,7 +49,13 @@ const FrontPage = ({ children }) => {
   useEffect(() => {
     if (mapRef.current) {
       if (ready) {
-        redrawElements(mapRef, currentStoryItem.points, currentStoryItem.lines, hovered, setHovered)
+        redrawElements(
+          mapRef,
+          currentStoryItem.points,
+          currentStoryItem.lines,
+          hovered,
+          setHovered
+        )
       } else {
         mapRef.current.graphics.removeAll()
       }
@@ -64,6 +71,41 @@ const FrontPage = ({ children }) => {
         transition={{ duration: 1 }}
         className={`h-screen w-screen relative`}
       >
+        {!ready && (
+          <>
+            <ScrollLink
+              to="research"
+              smooth={true}
+              duration={500}
+              className="absolute bottom-0 left-1/2 -ml-5 mb-10 z-30"
+            >
+              <ChevronDoubleDownIcon className="h-10 w-10" />
+            </ScrollLink>
+            <div className="absolute top-0 left-0 w-full flex flex-col md:flex-row md:justify-between py-3 md:py-5 px-5 md:px-10 z-30">
+              <div className="">
+                <div className="flex items-center space-x-1">
+                  <Logo className="h-6 w-6 mb-1" />
+                  <p className="text-xl">
+                    <span className="font-bold">CDN</span>Hatch
+                  </p>
+                </div>
+              </div>
+              <div className="hidden md:inline-block text-right text-xs md:text-sm">
+                <p className="">
+                  A submission for <a>Esri's Hack for a Sustainable Future</a>
+                </p>
+                <p className="">
+                  Built by <a>Sam Larsen-Disney</a> & <a>Yannis Panagis</a>
+                </p>
+                <div className="mt-2 hidden md:flex md:justify-end">
+                  <a href="" className="">
+                    <DevPost className="w-20 md:w-24" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
         {!isSSR && (
           <React.Suspense fallback={<div />}>
             <ClientSideOnlyMap
@@ -112,19 +154,19 @@ const FrontPage = ({ children }) => {
 
         {mapLoaded && !submitted && !ready && (
           <>
-            <div className="absolute z-30 h-full w-full top-0 left-0">
+            <div className="absolute z-30 h-full w-full top-0 left-0 px-2">
               <div className="flex flex-col items-center justify-center w-full h-full space-y-3">
-                <Logo className="h-12 w-12 text-yellow-400" />
-                <h1 className="text-4xl font-bold text-white max-w-lg text-center">
+                <Logo className="h-8 w-8 md:h-12 md:w-12 text-yellow-400" />
+                <h1 className="text-2xl md:text-4xl font-bold text-white max-w-lg text-center">
                   Optimizing CDN locations for a greener planet.
                 </h1>
-                <div className="border-4 border-yellow-400 rounded-full flex p-1 w-full max-w-lg">
+                <div className="border-4 border-yellow-400 rounded-full flex p-1 mx-4 w-full max-w-lg">
                   <input
                     value={userInput}
                     onChange={e => setUserInput(e.target.value)}
                     onKeyDown={e => e.key === "Enter" && submitURL(userInput)}
                     placeholder="your-website.com"
-                    className="w-full bg-transparent text-2xl text-white px-2 mx-2 focus:outline-none"
+                    className="w-full bg-transparent text-lg md:text-2xl text-white px-1 md:px-2 mx-2 focus:outline-none"
                   />
                   <button
                     onClick={() => {
@@ -132,68 +174,42 @@ const FrontPage = ({ children }) => {
                     }}
                     className="bg-yellow-400 hover:bg-yellow-500 rounded-full text-white p-2"
                   >
-                    <SearchIcon className="h-6 w-6" />
+                    <SearchIcon className="h-4 w-4 md:h-6 md:w-6" />
                   </button>
                 </div>
               </div>
             </div>
-            <ScrollLink
-              to="research"
-              smooth={true}
-              duration={500}
-              className="absolute bottom-0 left-1/2 -ml-5 mb-10 z-30"
-            >
-              <ChevronDoubleDownIcon className="h-10 w-10" />
-            </ScrollLink>
-            <div className="absolute top-0 left-0 text-right my-5 mx-10 z-30">
-              <div className="flex items-center space-x-1">
-                <Logo className="h-6 w-6 mb-1" />
-                <p className="text-xl">
-                  <span className="font-bold">CDN</span>Hatch
-                </p>
-              </div>
-            </div>
-            <div className="absolute top-0 right-0 text-right my-5 mx-10 z-30">
-              <p className="text-sm">
-                A submission for <a>Esri's Hack for a Sustainable Future</a>
-              </p>
-              <p className="text-sm">
-                Built by <a>Sam Larsen-Disney</a> & <a>Yannis Panagis</a>
-              </p>
-              <div className="flex mt-2 justify-end">
-                <a href="" className="">
-                  <DevPost className="w-24" />
-                </a>
-              </div>
-            </div>
           </>
         )}
+
         {(!mapLoaded || (submitted && !ready)) && (
           <div className="absolute z-30 h-full w-full top-0 left-0">
             <div className="flex flex-col items-center justify-center w-full h-full space-y-3">
-          
-                <svg
-                  class="animate-spin -ml-1 mr-3 h-24 w-24 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-              <p className="text-2xl font-bold">{!mapLoaded? "Preparing your experience...": "Creating the story..."}</p>
-            
+              <svg
+                className="animate-spin -ml-1 mr-3 h-16 w-16 md:h-24 md:w-24 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              <p className="text-lg md:text-2xl font-bold">
+                {!mapLoaded
+                  ? "Preparing your experience..."
+                  : "Creating the story..."}
+              </p>
             </div>
           </div>
         )}
