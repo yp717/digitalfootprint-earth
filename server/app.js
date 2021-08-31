ca79cdfdee1fc922ab9f1202079c9af8c2500c51
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-08-31 19:39:28 +0100

    feat(cache): added fb caching

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
-const dns = require("dns");
+require("dotenv").config()
+const crypto = require('crypto');
 const express = require("express");
 const fetch = require("node-fetch");
 const { handle } = require("./cdnHandeler");
 const app = express();
 var get_ip = require("ipware")().get_ip;
+
+var firebase = require("firebase-admin")
+
+var serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG)
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DB_URL,
+})
+
+const db = firebase.firestore();
+
 const port = process.env.PORT || 3000;
 var cors = require("cors");
 var whitelist = [
@@ -29,27 +42,68 @@ app.use(function (req, res, next) {
 app.get("/:url", async (req, res) => {
   var URL = req.params.url;
   let IP = !req.clientIpRoutable ? "51.9.166.141" : req.clientIp;
-  console.log(req.clientIp, req.clientIpRoutable);
+  const id = crypto.createHash(`md5`).update(`${URL}_${IP}`).digest(`hex`);
 
-  const userData = await fetch(`http://ip-api.com/json/${IP}`).then((res) =>
-    res.json()
-  );
-  const data = await fetch(`http://ip-api.com/json/${URL}`).then((res) =>
-    res.json()
-  );
-  const greenWebFoundation = await fetch(
-    `https://admin.thegreenwebfoundation.org/api/v3/greencheck/${URL}`
-  ).then((res) => res.json());
-  handle(
-    {
+  const userDocRef = await db.collection("stories").doc(id);
+  const doc = await userDocRef.get();
+  if (doc.exists) {
+    console.log("Story Requested Already!");
+    // if cache - > redirect to URL
+    const { locked, data } = doc.data();
+    // if in progress -> wait -> redirect to URL
+    if (locked) {
+      console.log("Waiting for story");
+      userDocRef.onSnapshot(
+        (doc) => {
+          const { locked, data } = doc.data();
+          if (!locked) {
+            res.send(data);
+          }
+        },
+        (err) => {
+          res.sendStatus(500);
+          res.end();
+          return;
+        }
+      );
+    } else {
+      // Story is already ready -> Send it!
+      res.send(data);
+      return;
+    }
+  } else {
+    db.collection("stories").doc(id).set(
+      {
+        locked: true,
+      },
+      { merge: true }
+    );
+    // Create the Story
+    const userData = await fetch(`http://ip-api.com/json/${IP}`).then((res) =>
+      res.json()
+    );
+    const data = await fetch(`http://ip-api.com/json/${URL}`).then((res) =>
+      res.json()
+    );
+    const greenWebFoundation = await fetch(
+      `https://admin.thegreenwebfoundation.org/api/v3/greencheck/${URL}`
+    ).then((res) => res.json());
+    const DataWithCDN = handle({
       requestData: { ...data, url: URL },
       userInfo: userData,
       environmentalData: {
         greenWebFoundation,
       },
-    },
-    res
-  );
+    });
+    res.send(DataWithCDN);
+    db.collection("stories").doc(id).set(
+      {
+        locked: false,
+        data: DataWithCDN,
+      },
+      { merge: true }
+    );
+  }
 });
 
 app.listen(port, () => {
